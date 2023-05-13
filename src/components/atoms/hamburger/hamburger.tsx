e24e6a7a85497f6616bf9600822e376a4519f62d
leonardo-junior <leoleo1@hotmail.com>
2023-05-13 15:25:51 -0300

    feat: add mobile nav

---
+++ src/components/atoms/hamburger/hamburger.tsx
@@ -0,0 +1,11 @@
+import styles from './hamburger.module.scss'
+
+export const Hamburger = () => {
+  return (
+    <button className={styles.container}>
+      <span />
+      <span />
+      <span />
+    </button>
+  )
+}
--- src/components/organisms/nav/nav.tsx
+++ src/components/organisms/nav/nav.tsx
@@ -1,5 +1,20 @@
+import { Hamburger } from 'components/atoms/hamburger/hamburger'
 import styles from './nav.module.scss'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
 
 export const Nav = () => {
-  return <div className={styles.container}>nav</div>
+  return (
+    <div className={styles.container}>
+      <Hamburger />
+
+      <img src="/logo-estado-de-minas.svg" className={styles.logo} />
+
+      <div className={`${styles.signIt} ${roboto.className}`}>Assine</div>
+    </div>
+  )
 }

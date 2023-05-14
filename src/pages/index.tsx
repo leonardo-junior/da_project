a71d34ceab386505fbb30857746a5d15440a2156
leonardo-junior <leoleo1@hotmail.com>
2023-05-14 20:46:04 -0300

    feat: improve meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,13 @@ export default function HomePage() {
         <title>Estado de Minas | Notícias</title>
         <meta name="description" content="Página principal de notícias de Minas Gerais" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Acompanhe as últimas notícias e fique bem informado o que acontece em Minas Gerais."
+        />
+        <meta name="robots" content="index,follow" />
+        <meta name="author" content="Tamaki Ryushi" />
+        <meta name="keywords" content="minas gerais, notícias, novidades"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
